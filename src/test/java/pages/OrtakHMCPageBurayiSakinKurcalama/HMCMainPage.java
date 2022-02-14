a64f5e20d9277960e3eedfd40e1ddfaf12e11a3d
Mustafa DONAT <mustafadonat00@gmail.com>
2022-02-15 00:47:38 +0300

    j

--- src/test/java/pages/OrtakHMCPageBurayiSakinKurcalama/HMCMainPage.java
+++ src/test/java/pages/OrtakHMCPageBurayiSakinKurcalama/HMCMainPage.java
@@ -31,7 +31,7 @@ public HMCMainPage(){
     @FindBy(xpath = "//input[@id='UserName']")
     public WebElement userNameBox;
 
-    @FindBy(id = "Password")
+    @FindBy(id = "Password")git
     public WebElement passwordBox;
 
     @FindBy(id = "btnSubmit")
